983c9720c1dbbed36ffb3e254ceb799a9ab7ccd6
Michael Birken <o__1@hotmail.com>
2024-04-25 22:48:00 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import sharp from 'sharp';
 
-const GLYPHS_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/printable-ascii-bw.png';
-const INPUT_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/circle.png';
+const GLYPHS_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/printable-ascii-bw-2.png';
+const INPUT_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/xbox.png';
 
 const PRINTABLE_ASCII
     = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
@@ -9,10 +9,20 @@ const PRINTABLE_ASCII
 class Glyph {
     character: string;
     pixels: boolean[][];
+    count: number;
 
     constructor(character: string, pixels: boolean[][]) {
         this.character = character;
         this.pixels = pixels;
+        this.count = 0;
+        for (let i = pixels.length - 1; i >= 0; --i) {
+            const row = pixels[i];
+            for (let j = row.length - 1; j >= 0; --j) {
+                if (row[j]) {
+                    ++this.count;
+                }
+            }
+        }
     }
 }
 
@@ -27,31 +37,92 @@ async function loadGlyphs(): Promise<Glyph[]> {
     const glyphWidth = metadata.width / PRINTABLE_ASCII.length;
     const glyphHeight = metadata.height;
 
-    console.log(`${glyphWidth} ${glyphHeight}`);
-
     const glyphs = new Array<Glyph>(PRINTABLE_ASCII.length);
+    const imagePixels = await image.raw().toColourspace('b-w').toBuffer();
     for (let i = PRINTABLE_ASCII.length - 1; i >= 0; --i) {
-        const pixels = new Array<boolean[]>(glyphHeight);
-        for (let j = glyphHeight - 1; j >= 0; --j) {
-            pixels[j] = new Array<boolean>(glyphWidth);
-        }
-        glyphs[i] = new Glyph(PRINTABLE_ASCII[i], pixels);
-    }
-
-    const pixels = await image.raw().toColourspace('b-w').toBuffer();
-    for (let i = glyphs.length - 1; i >= 0; --i) {
-        const glyph = glyphs[i];
+        const glphyPixels = new Array<boolean[]>(glyphHeight);
         const I = i * glyphWidth;
-        for (let j = glyph.pixels.length - 1; j >= 0; --j) {
+        for (let j = glyphHeight - 1; j >= 0; --j) {
+            const row = glphyPixels[j] = new Array<boolean>(glyphWidth);
             const J = I + j * metadata.width;
-            const row = glyph.pixels[j];
             for (let k = row.length - 1; k >= 0; --k) {
-                row[k] = pixels[J + k] !== 0;
+                row[k] = (imagePixels[J + k] !== 0);
             }
         }
+        glyphs[i] = new Glyph(PRINTABLE_ASCII[i], glphyPixels);
     }
+
+    glyphs.sort((a, b) => a.count - b.count);
 
     return glyphs;
 }
 
-await loadGlyphs();
+async function loadPaddedImage(glyphWidth: number, glyphHeight: number): Promise<boolean[][]> {
+    const image = sharp(INPUT_IMAGE_FILENAME);
+
+    const metadata = await image.metadata();
+    if (!metadata.width || !metadata.height) {
+        throw new Error('Image dimensions not available');
+    }
+
+    const width = Math.ceil(metadata.width / glyphWidth) * glyphWidth;
+    const height = Math.ceil(metadata.height / glyphHeight) * glyphHeight;
+    const xOffset = Math.floor((width - metadata.width) / 2);
+    const yOffset = Math.floor((height - metadata.height) / 2);
+
+    const data = new Array<boolean[]>(height);
+    for (let y = height - 1; y >= 0; --y) {
+        data[y] = new Array<boolean>(width).fill(false);
+    }
+
+    const pixels = await image.raw().toColourspace('b-w').toBuffer();
+    for (let y = metadata.height - 1; y >= 0; --y) {
+        const Y = metadata.width * y;
+        const row = data[y + yOffset];
+        for (let x = metadata.width - 1; x >= 0; --x) {
+            row[x + xOffset] = (pixels[Y + x] >= 64);
+        }
+    }
+
+    return data;
+}
+
+function findBestGlyph(glyphs: Glyph[], glyphWidth: number, glyphHeight: number, imagePixels: boolean[][],
+                       xOffset: number, yOffset: number): Glyph {
+    let best = 0;
+    outer: for (let i = glyphs.length - 1; i >= 0; --i) {
+        const glyph = glyphs[i];
+        const glyphPixels = glyph.pixels;
+        for (let y = glyphHeight - 1; y >= 0; --y) {
+            const glyphRow = glyphPixels[y];
+            const imageRow = imagePixels[y + yOffset];
+            for (let x = glyphWidth - 1; x >= 0; --x) {
+                if (glyphRow[x] && !imageRow[x + xOffset]) {
+                    continue outer;
+                }
+            }
+        }
+        best = i;
+        break;
+    }
+    return glyphs[best];
+}
+
+function convertImage(glyphs: Glyph[], glyphWidth: number, glyphHeight: number, imagePixels: boolean[][]) {
+    for (let y = 0; y < imagePixels.length; y += glyphHeight) {
+        let s = '';
+        for (let x = 0; x < imagePixels[y].length; x += glyphWidth) {
+            const glyph = findBestGlyph(glyphs, glyphWidth, glyphHeight, imagePixels, x, y);
+            s += glyph.character;
+        }
+        console.log(s);
+    }
+}
+
+const glyphs = await loadGlyphs();
+const glyphWidth = glyphs[0].pixels[0].length;
+const glyphHeight = glyphs[0].pixels.length;
+
+const imagePixels = await loadPaddedImage(glyphWidth, glyphHeight);
+convertImage(glyphs, glyphWidth, glyphHeight, imagePixels);
+
