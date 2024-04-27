651988eeab16cf47bb9e10a077b4af9c1c8dec43
Michael Birken <o__1@hotmail.com>
2024-04-27 13:22:24 -0400

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import sharp from 'sharp';
 import chroma from 'chroma-js';
+import * as ss from 'simple-statistics';
+import * as os from 'os';
 
 const GLYPHS_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/printable-ascii-bw-2.png';
-const INPUT_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/xbox.png';
+const INPUT_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/ms-office.png';
 const PALETTE_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/palette.png';
+
+const BLACK_LUMINANCE = 0.1;
+
+const SCALED_GLYPH_WIDTH = 9;
+const SCALED_GLYPH_HEIGHT = 19;
 
 const PRINTABLE_ASCII
     = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
@@ -77,24 +84,31 @@ const palette = await (async () => {
 })();
 
 const closestColorCache = new Map<number, number>();
-function findClosestColor(r: number, g: number, b: number) {
-    const key = (r << 16) | (g << 8) | b;
+function findClosestColor(r: number, g: number, b: number, a: number) {
+    const key = (r << 24) | (g << 16) | (b << 8) | a;
     const value = closestColorCache.get(key);
     if (value !== undefined) {
         return value;
     }
+    const alpha = a / 255;
 
-    let index = 256;
-    let error = Number.MAX_VALUE;
-    for (let i = 255; i >= 16; --i) {
-        const p = palette[i];
-        const dr = p[0] - r;
-        const dg = p[1] - g;
-        const db = p[2] - b;
-        const e = dr * dr + dg * dg + db * db;
-        if (e < error) {
-            error = e;
-            index = i;
+    let index = 0;
+    const c = chroma(r, g, b).lab();
+    c[0] *= alpha;
+    c[1] *= alpha;
+    c[2] *= alpha;
+    if (c[0] >= BLACK_LUMINANCE) {
+        let error = Number.MAX_VALUE;
+        for (let i = 255; i >= 16; --i) {
+            const p = palette[i];
+            const dl = p[0] - c[0];
+            const da = p[1] - c[1];
+            const db = p[2] - c[2];
+            const e = dl * dl + da * da + db * db;
+            if (e < error) {
+                error = e;
+                index = i;
+            }
         }
     }
 
@@ -127,80 +141,128 @@ async function loadInputImage(): Promise<Image> {
     const image = sharp(INPUT_IMAGE_FILENAME);
     const { data, info } = await image.raw().toBuffer({ resolveWithObject: true });
     const indices = new Uint8Array(info.width * info.height);
-    for (let i = 0, j = 0; i < indices.length; ++i) {
-        indices[i] = findClosestColor(data[j++], data[j++], data[j++]);
+    switch (info.channels) {
+        case 1:
+            for (let i = 0; i < indices.length; ++i) {
+                indices[i] = findClosestColor(data[i], data[i], data[i], 0xFF);
+            }
+            break;
+        case 2:
+            for (let i = 0, j = 0; i < indices.length; ++i) {
+                indices[i] = findClosestColor(data[j], data[j], data[j++], data[j++]);
+            }
+            break;
+        case 3:
+            for (let i = 0, j = 0; i < indices.length; ++i) {
+                indices[i] = findClosestColor(data[j++], data[j++], data[j++], 0xFF);
+            }
+            break;
+        case 4:
+            for (let i = 0, j = 0; i < indices.length; ++i) {
+                indices[i] = findClosestColor(data[j++], data[j++], data[j++], data[j++]);
+            }
+            break;
     }
     return new Image(indices, info.width, info.height);
 }
 
+class Ascii {
+    text: string;
+    matched: number;
 
-
-async function loadPaddedImage(glyphWidth: number, glyphHeight: number): Promise<boolean[][]> {
-    const image = sharp(INPUT_IMAGE_FILENAME);
-
-    const metadata = await image.metadata();
-    if (!metadata.width || !metadata.height) {
-        throw new Error('Image dimensions not available');
+    constructor(text: string, matched: number) {
+        this.text = text;
+        this.matched = matched;
     }
-
-    const width = Math.ceil(metadata.width / glyphWidth) * glyphWidth;
-    const height = Math.ceil(metadata.height / glyphHeight) * glyphHeight;
-    const xOffset = Math.floor((width - metadata.width) / 2);
-    const yOffset = Math.floor((height - metadata.height) / 2);
-
-    const data = new Array<boolean[]>(height);
-    for (let y = height - 1; y >= 0; --y) {
-        data[y] = new Array<boolean>(width).fill(false);
-    }
-
-    const pixels = await image.raw().toColourspace('b-w').toBuffer();
-    for (let y = metadata.height - 1; y >= 0; --y) {
-        const Y = metadata.width * y;
-        const row = data[y + yOffset];
-        for (let x = metadata.width - 1; x >= 0; --x) {
-            row[x + xOffset] = (pixels[Y + x] >= 64);
-        }
-    }
-
-    return data;
 }
 
-function findBestGlyph(glyphs: Glyph[], glyphWidth: number, glyphHeight: number, imagePixels: boolean[][],
-                       xOffset: number, yOffset: number): Glyph {
-    let best = 0;
-    outer: for (let i = glyphs.length - 1; i >= 0; --i) {
-        const glyph = glyphs[i];
-        const glyphPixels = glyph.pixels;
-        for (let y = glyphHeight - 1; y >= 0; --y) {
-            const glyphRow = glyphPixels[y];
-            const imageRow = imagePixels[y + yOffset];
-            for (let x = glyphWidth - 1; x >= 0; --x) {
-                if (glyphRow[x] && !imageRow[x + xOffset]) {
-                    continue outer;
+function convert(image: Image, offsetX: number, offsetY: number): Ascii {
+
+    const cols = Math.ceil(image.width / SCALED_GLYPH_WIDTH);
+    const rows = Math.ceil(image.height / SCALED_GLYPH_HEIGHT);
+    const paddedWidth = Math.ceil(cols * SCALED_GLYPH_WIDTH);
+    const paddedHeight = Math.ceil(rows * SCALED_GLYPH_HEIGHT);
+    const originX = offsetX + (image.width - paddedWidth) / 2;
+    const originY = offsetY + (image.height - paddedHeight) / 2;
+    const indexArray = new Array<number>();
+    const glyphScaleX = SCALED_GLYPH_WIDTH / glyphWidth;
+    const glyphScaleY = SCALED_GLYPH_HEIGHT / glyphHeight;
+
+    let text = '';
+    let lastMedianIndex = -1;
+    let matched = 0;
+    for (let r = 0; r < rows; ++r) {
+        const glyphOriginY = originY + SCALED_GLYPH_HEIGHT * r;
+        for (let c = 0; c < cols; ++c) {
+            const glyphOriginX = originX + SCALED_GLYPH_WIDTH * c;
+
+            indexArray.length = 0;
+            for (let y = 0; y < glyphHeight; ++y) {
+                const glyphY = glyphOriginY + glyphScaleY * y;
+                for (let x = 0; x < glyphWidth; ++x) {
+                    const glyphX = glyphOriginX + glyphScaleX * x;
+                    const index = image.getIndex(glyphX, glyphY);
+                    if (index !== 0) {
+                        indexArray.push(index);
+                    }
                 }
             }
+            if (indexArray.length === 0) {
+                text += ' ';
+                continue;
+            }
+            const medianIndex = ss.median(indexArray);
+            if (lastMedianIndex !== medianIndex) {
+                text += `\x1b[38;5;${medianIndex}m`;
+                lastMedianIndex = medianIndex;
+            }
+
+            let glyphIndex = 0;
+            outer: for (let i = glyphs.length - 1; i >= 0; --i) {
+                const glyph = glyphs[i];
+                const glyphPixels = glyph.pixels;
+                for (let y = 0; y < glyphHeight; ++y) {
+                    const glyphRow = glyphPixels[y];
+                    const glyphY = glyphOriginY + glyphScaleY * y;
+                    for (let x = 0; x < glyphWidth; ++x) {
+                        const glyphX = glyphOriginX + glyphScaleX * x;
+                        const index = image.getIndex(glyphX, glyphY);
+                        if (glyphRow[x] && index !== medianIndex) {
+                            continue outer;
+                        }
+                    }
+                }
+                glyphIndex = i;
+                break;
+            }
+            text += glyphs[glyphIndex].character;
+            matched += glyphs[glyphIndex].count;
         }
-        best = i;
-        break;
+        text += os.EOL;
     }
-    return glyphs[best];
+    text += '\x1b[0m';
+
+    return new Ascii(text, matched);
 }
 
-function convertImage(glyphs: Glyph[], glyphWidth: number, glyphHeight: number, imagePixels: boolean[][]) {
-    for (let y = 0; y < imagePixels.length; y += glyphHeight) {
-        let s = '';
-        for (let x = 0; x < imagePixels[y].length; x += glyphWidth) {
-            const glyph = findBestGlyph(glyphs, glyphWidth, glyphHeight, imagePixels, x, y);
-            s += glyph.character;
+function sweepConvert(image: Image): Ascii {
+    let ascii = new Ascii('', 0);
+    for (let y = 0; y < glyphHeight; ++y) {
+        for (let x = 0; x < glyphWidth; ++x) {
+            const a = convert(image, x, y);
+            if (a.matched > ascii.matched) {
+                ascii = a;
+            }
         }
-        console.log(s);
     }
+    return ascii;
 }
 
+const image = await loadInputImage();
 
 
+const ascii = sweepConvert(image);
 
-//
-// const imagePixels = await loadPaddedImage(glyphWidth, glyphHeight);
-// convertImage(glyphs, glyphWidth, glyphHeight, imagePixels);
+console.log(ascii.text);
+console.log(ascii.matched);
 
