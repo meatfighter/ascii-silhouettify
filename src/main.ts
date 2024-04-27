ddc512ac617f7f1f68573d05b72928f0b3ee1ff2
Michael Birken <o__1@hotmail.com>
2024-04-26 22:46:09 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import sharp from 'sharp';
+import chroma from 'chroma-js';
 
 const GLYPHS_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/printable-ascii-bw-2.png';
 const INPUT_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/xbox.png';
+const PALETTE_IMAGE_FILENAME = 'C:/js-projects/ascii-silhouette/images/palette.png';
 
 const PRINTABLE_ASCII
     = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
@@ -26,7 +28,7 @@ class Glyph {
     }
 }
 
-async function loadGlyphs(): Promise<Glyph[]> {
+const glyphs = await (async () => {
     const image = sharp(GLYPHS_IMAGE_FILENAME);
 
     const metadata = await image.metadata();
@@ -38,7 +40,7 @@ async function loadGlyphs(): Promise<Glyph[]> {
     const glyphHeight = metadata.height;
 
     const glyphs = new Array<Glyph>(PRINTABLE_ASCII.length);
-    const imagePixels = await image.raw().toColourspace('b-w').toBuffer();
+    const imagePixels = await image.raw().toColourspace('b-w').toBuffer(); // TODO toBuffer can also return the metadata
     for (let i = PRINTABLE_ASCII.length - 1; i >= 0; --i) {
         const glphyPixels = new Array<boolean[]>(glyphHeight);
         const I = i * glyphWidth;
@@ -55,7 +57,83 @@ async function loadGlyphs(): Promise<Glyph[]> {
     glyphs.sort((a, b) => a.count - b.count);
 
     return glyphs;
+})();
+const glyphWidth = glyphs[0].pixels[0].length;
+const glyphHeight = glyphs[0].pixels.length;
+
+const palette = await (async () => {
+    const image = sharp(PALETTE_IMAGE_FILENAME);
+    const imagePixels = await image.raw().toBuffer();
+
+    const palette = new Array<number[]>(256);
+    for (let y = 0, p = 0; y < 16; ++y) {
+        let Y = (9 + y * 19) * 144;
+        for (let x = 0; x < 16; ++x) {
+            let X = (4 + x * 9 + Y) * 3;
+            palette[p++] = chroma(imagePixels[X], imagePixels[X + 1], imagePixels[X + 2]).lab();
+        }
+    }
+    return palette;
+})();
+
+const closestColorCache = new Map<number, number>();
+function findClosestColor(r: number, g: number, b: number) {
+    const key = (r << 16) | (g << 8) | b;
+    const value = closestColorCache.get(key);
+    if (value !== undefined) {
+        return value;
+    }
+
+    let index = 256;
+    let error = Number.MAX_VALUE;
+    for (let i = 255; i >= 16; --i) {
+        const p = palette[i];
+        const dr = p[0] - r;
+        const dg = p[1] - g;
+        const db = p[2] - b;
+        const e = dr * dr + dg * dg + db * db;
+        if (e < error) {
+            error = e;
+            index = i;
+        }
+    }
+
+    closestColorCache.set(key, index);
+    return index;
 }
+
+class Image {
+    indices: Uint8Array;
+    width: number;
+    height: number;
+
+    constructor(indices: Uint8Array, width: number, height: number) {
+        this.indices = indices;
+        this.width = width;
+        this.height = height;
+    }
+
+    getIndex(x: number, y: number) {
+        const X = Math.round(x);
+        const Y = Math.round(y);
+        if (X < 0 || Y < 0 || X >= this.width || Y >= this.height) {
+            return 0;
+        }
+        return this.indices[this.width * Y + X];
+    }
+}
+
+async function loadInputImage(): Promise<Image> {
+    const image = sharp(INPUT_IMAGE_FILENAME);
+    const { data, info } = await image.raw().toBuffer({ resolveWithObject: true });
+    const indices = new Uint8Array(info.width * info.height);
+    for (let i = 0, j = 0; i < indices.length; ++i) {
+        indices[i] = findClosestColor(data[j++], data[j++], data[j++]);
+    }
+    return new Image(indices, info.width, info.height);
+}
+
+
 
 async function loadPaddedImage(glyphWidth: number, glyphHeight: number): Promise<boolean[][]> {
     const image = sharp(INPUT_IMAGE_FILENAME);
@@ -119,10 +197,10 @@ function convertImage(glyphs: Glyph[], glyphWidth: number, glyphHeight: number, 
     }
 }
 
-const glyphs = await loadGlyphs();
-const glyphWidth = glyphs[0].pixels[0].length;
-const glyphHeight = glyphs[0].pixels.length;
 
-const imagePixels = await loadPaddedImage(glyphWidth, glyphHeight);
-convertImage(glyphs, glyphWidth, glyphHeight, imagePixels);
+
+
+//
+// const imagePixels = await loadPaddedImage(glyphWidth, glyphHeight);
+// convertImage(glyphs, glyphWidth, glyphHeight, imagePixels);
 
