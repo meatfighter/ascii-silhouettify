b3068f7901310181430cd61cdb1ff15b6da4db62
Michael Birken <o__1@hotmail.com>
2024-04-28 15:45:04 -0400

    backup

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,20 @@
 
 import { convert } from '@/ascii';
 import { loadImage } from '@/images';
+import console from 'console';
+
+function printUsage() {
+    console.log();
+    console.log('Usage: ascii-silhouette [options]');
+    console.log();
+    console.log('Options:');
+    console.log('  -i, --input ...     Input filename (J')
+    console.log('  -o, --output ...    Output filename (in quotes)');
+    console.log();
+    console.log('  -v, --version       Print ascii-silhouette version');
+    console.log('  -h, --help          Print ascii-silhouette command-line options');
+    console.log();
+}
 
 const ascii = convert(await loadImage('images/ubuntu.png'), true, 1, 12, 1.2, false);
 
