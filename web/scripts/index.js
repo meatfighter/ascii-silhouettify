feab97b61ab97b1575c75b0bd3c4d21da4789846
Michael Birken <o__1@hotmail.com>
2024-06-02 15:47:51 -0400

    update

---
+++ web/scripts/index.js
@@ -0,0 +1,16 @@
+function init() {
+    const buttonContainer = document.querySelector(".button-container");
+    const buttons = buttonContainer.querySelectorAll(".button");
+    let maxWidth = 0;
+
+    buttons.forEach(button => {
+        const buttonWidth = button.offsetWidth;
+        if (buttonWidth > maxWidth) {
+            maxWidth = buttonWidth;
+        }
+    });
+
+    buttons.forEach(button => button.style.width = `${maxWidth}px`);
+}
+
+document.addEventListener("DOMContentLoaded", init);
