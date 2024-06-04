91531bfa62a9a3a8fd95661a6fe6a959312c7f6e
Michael Birken <o__1@hotmail.com>
2024-06-04 12:33:50 -0400

    update

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,9 +1,5 @@
-function copyToClipboard(button) {
-    void navigator.clipboard.writeText(button.parentElement.querySelector('pre').innerText.trim());
-}
-
-function init() {
-    const buttons = document.querySelector(".button-container").querySelectorAll(".button");
+function makeButtonsSameWidth() {
+    const buttons = document.querySelector('.button-container').querySelectorAll('.button');
     let maxWidth = 0;
 
     buttons.forEach(button => {
@@ -14,6 +10,30 @@ function init() {
     });
 
     buttons.forEach(button => button.style.width = `${maxWidth}px`);
+}
+
+function copyToClipboard(button) {
+    void navigator.clipboard.writeText(button.parentElement.querySelector('pre').innerText.trim());
+}
+
+function goToPage(url) {
+    window.location.href = url;
+}
+
+function initButtonListeners() {
+    document.querySelectorAll('.copy-btn').forEach(button => button.addEventListener('click',
+            () => copyToClipboard(button)))
+    document.getElementById('launch-browser-version-button').addEventListener('click',
+            () => goToPage('spa/index.html'));
+    document.getElementById('color-gallery-button').addEventListener('click',
+            () => goToPage('color-gallery.html'));
+    document.getElementById('monochrome-gallery-button').addEventListener('click',
+            () => goToPage('monochrome-gallery.html'));
+}
+
+function init() {
+    makeButtonsSameWidth();
+    initButtonListeners();
 }
 
 document.addEventListener("DOMContentLoaded", init);
