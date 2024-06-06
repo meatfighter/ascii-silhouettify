a5b24bf14f36a0b2029fcfd488358315246214a5
Michael Birken <o__1@hotmail.com>
2024-06-05 22:33:09 -0400

    update

--- src/html.ts
+++ src/html.ts
@@ -3,7 +3,7 @@ export function getHtmlHeader(title: string, fontSize: number, lineHeight: numbe
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#0C0C0C">
     <meta name="date" content="${new Date().toISOString()}">
     <link href="https://fonts.cdnfonts.com/css/cascadia-code" rel="stylesheet">
@@ -17,19 +17,12 @@ export function getHtmlHeader(title: string, fontSize: number, lineHeight: numbe
         background: #0C0C0C;
         color: #CCCCCC;
         text-align: center;
-        margin: 0;
-        padding: 0;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
       }
       pre {
         font-family: 'Cascadia Code', sans-serif;
         font-size: ${fontSize}pt; 
         line-height: ${lineHeight};
-        margin: auto;
-      }       
+      }        
     </style>    
 </head>
 <body>
